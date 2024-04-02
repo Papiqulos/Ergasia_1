292f8371eeb1cb6301a8c6da453e50e0450836b6
George Papoutsas <osfpgeopap1925@gmail.com>
2024-04-02 21:52:03 +0300

    init commit

---
+++ Askhsh_1/askhsh_1_a_b.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Contraint functions
+def f1(x): # >=
+    return 10 - x
+
+def f2(x): # <=
+    return 10 + 10 * x
+
+def f3(x):  # <=
+    return 20 + 4 * x
+
+def f4(x): # >=
+    return 5 - x / 4
+
+# Cptimization functions
+def fz1(x1, x2):
+    return 2 * x1 - x2
+
+def fz2(x1, x2):
+    return 11 * x1 - x2
+
+# Define the constraints
+d = np.linspace(0, 35, 300)
+x1, x2 = np.meshgrid(d, d)
+
+x = np.linspace(-100, 100, 3000)
+
+constraint1 = x1 + x2 >= 10
+constraint2 = -10 * x1 + x2 <= 10
+constraint3 = -4 * x1 + x2 <= 20
+constraint4 = x1 + 4 * x2 >= 20
+constraint5 = x1 >= 0
+constraint6 = x2 >= 0
+
+# Find the intersection points of the constraints
+idx = np.argwhere(np.diff(np.sign(f1(x) - f2(x)))).flatten()
+intersection1 = [x[idx][0], f1(x)[idx][0]]
+
+idx = np.argwhere(np.diff(np.sign(f2(x) - f3(x))).flatten())
+intersection2 = [x[idx][0], f2(x)[idx][0]]
+
+idx = np.argwhere(np.diff(np.sign(f1(x) - f4(x)))).flatten()
+intersection3 = [x[idx][0], f1(x)[idx][0]]
+
+intersection4 = [20, 0]
+
+# Erwthma a
+intersections = [intersection1, intersection2, intersection3, intersection4]
+print(intersections)
+index = 0
+minima = 0
+for i, intersection in enumerate(intersections):
+    z = fz1(intersection[0], intersection[1])
+    if z < minima:
+        minima = z
+        index = i
+
+print(f"The minimum value of the function is: {minima} at point {intersections[index]} ")
+        
+
+# Erwthma b
+index = 0
+minima = 0
+for i, intersection in enumerate(intersections):
+    z = fz2(intersection[0], intersection[1])
+    if z < minima:
+        minima = z
+        index = i
+
+print(f"The minimum value of the function is: {minima} at point {intersections[index]} ")
+
+## Plots
+# Feasible region
+plt.imshow( ((constraint1 & constraint2 & constraint3 & constraint4 & constraint5 & constraint6) ).astype(int), 
+                extent=(x1.min(), x1.max(), x2.min(), x2.max()), origin="lower", cmap="Greys", alpha = 0.3)
+
+# Constraint plots
+plt.plot(x, f1(x), label='x1 + x2 >= 10')
+plt.plot(x, f2(x), label='-10x1 + x2 <= 10')
+plt.plot(x, f3(x), label='-4x1 + x2 <= 20')
+plt.plot(x, f4(x), label='x1 + 4x2 >= 20')
+
+# Plot the intersection points
+plt.plot(intersection1[0], intersection1[1], 'ro')
+plt.plot(intersection2[0], intersection2[1], 'ro')
+plt.plot(intersection3[0], intersection3[1], 'ro')
+plt.plot(intersection4[0], intersection4[1], 'ro')
+
+# Sauces
+plt.xlim(-1, 35)
+plt.ylim(-1, 35)
+plt.legend()
+plt.show()
+
+
+
+
+
